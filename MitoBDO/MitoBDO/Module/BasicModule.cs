3736aa328c3202773401ced1b1f7fc8edea33baa
EKN <sersen28@daum.net>
2023-02-24 14:26:55 +0900

    테스트 코드 삭제

--- MitoBDO/MitoBDO/Module/BasicModule.cs
+++ MitoBDO/MitoBDO/Module/BasicModule.cs
@@ -4,7 +4,7 @@ namespace MitoBDO.Module
 {
 	public class BasicModule : ModuleBase<SocketCommandContext>
 	{
-		[Command("test"), Alias("TEST")]
+		[Command("hi"), Alias("hello", "안녕", "ㅎㅇ")]
 		[Summary("Bot say \"Hello, World\"")]
 		public async Task test()
 			=> await Context.Channel.SendMessageAsync("Hello, World!");
