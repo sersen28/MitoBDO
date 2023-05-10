ab7ea564cccbbf5021e9b896375782252518aa56
EKN <sersen28@daum.net>
2023-05-10 13:12:56 +0900

    아이템 카테고리 추가

---
+++ MitoBDO/MitoBDO/Constants/ItemCategory/ArmorSubCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace MitoBDO.Constants.ItemCategory
+{
+	public static class ArmorSubCategory
+	{
+		public const uint Helmet = 1;
+		public const uint Armor = 2;
+		public const uint Glove = 3;
+		public const uint Shoes = 4;
+		public const uint Functional = 5;
+		public const uint Crafted = 6;
+	}
+}
---
+++ MitoBDO/MitoBDO/Constants/ItemCategory/ItemMainCategory.cs
@@ -0,0 +1,23 @@
+﻿namespace MitoBDO.Constants.ItemCategory
+{
+	public static class ItemMainCategory
+    {
+        public const uint MainWeapon = 1;
+		public const uint SubWeapon = 5;
+		public const uint AwakenWeapon = 10;
+        public const uint Armor = 15;
+        public const uint Accessory = 20;
+        public const uint Material = 25;
+		public const uint Enhancement = 30;
+        public const uint Consumable = 35;
+        public const uint LifeTools = 40;
+        public const uint AlchemicStone = 45;
+        public const uint MagicCrystal = 50;
+        public const uint PearlGoods = 55;
+        public const uint Dye = 60;
+        public const uint Animal = 65;
+        public const uint Wagon = 70;
+        public const uint Furniture = 75;
+        public const uint Lightstone = 80;
+	}
+}
